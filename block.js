1c8c6b8843724ee5934aff23b9b9883b0901341f
Kyle Phillips <khillips375@yahoo.com>
2020-02-10 11:24:01 -0500

    Added previous Hash

--- block.js
+++ block.js
@@ -1,15 +1,16 @@
 const { SHA256} = require('crypto-js');
 
 class Block {
-    constructor(data,index,timestamp = String(new Date())) {
+    constructor(data,index,timestamp = String(new Date()),previousHash) {
         this.date = data
         this.index = index
         this.timestamp = timestamp
         this.hash = this.calculateHash();
+        this.previousHash = previousHash
     }
     // Hashing Algorithm
     calculateHash() {
-        return SHA256(JSON.stringify(this.data) + this.index + this.timestamp).toString()
+        return SHA256(JSON.stringify(this.data) + this.index + this.timestamp + this.previousHash).toString()
     }
 
 }
